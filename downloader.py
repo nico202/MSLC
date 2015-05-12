3fb0d26be9e49daefc44eedc164d63cea5d71629
Nicolò Balzarotti <anothersms@gmail.com>
2015-05-12 22:11:28 +0200

    First filename definition (we then will merge all them in a single file)

---
+++ downloader.py
@@ -0,0 +1,4 @@
+#This script will download all the lyrics from the given site(s) and get info (BPM, genre, etc) about them
+#Output lyrics as .txt files on ./lyrics
+#We need to define a way to attach info in that file
+#And which site are we going to use
