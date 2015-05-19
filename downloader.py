5d2bb31793dc2594f95d11522ed77dfcc3f3ad9e
Nicolò Balzarotti <anothersms@gmail.com>
2015-05-19 21:27:24 +0200

    First working alpha

--- downloader.py
+++ downloader.py
@@ -1,38 +1,49 @@
 #This script will download all the lyrics from the given site(s) and get info (BPM, genre, etc) about them
-#Output lyrics as .txt files on ./lyrics
-#We need to define a way to attach info in that file
-#And which site are we going to use
+#Save all to a db
 from lxml import html
+import urllib
 import requests
 import datetime
 import sqlite3
 import os.path
 import sys
+import ast
+
+network_error_message = "Network error, try again"
+
+def count_letters(word):
+    BAD_LETTERS = " "
+    return len([letter for letter in word if letter not in BAD_LETTERS])
+
+def clear_word(word):
+    TO_REMOVE = '''()?![]{}=,;:.-+*/\\|'''
+    return "".join([letter for letter in word if letter not in TO_REMOVE])
 
 try:
     script, task, item = sys.argv
 except:
     script = sys.argv[0]
     print "Usage: %s [download|update|analyse] [hits|lyrics]" % (script)
+    print "Usage generate: %s generate starting_word" % (script)
     exit()
 
 is_new_session = not os.path.isfile('lyricsdb.sqlite')
 
 db = sqlite3.connect('lyricsdb.sqlite')
 c = db.cursor()
-
+d = db.cursor()
 if is_new_session:
     c.execute('''CREATE TABLE hits
-                 (year int, artist text, name text, number int, month int)
+                 (year int, artist text, title text, number int, month int)
             ''')
     c.execute('''CREATE TABLE lyrics
-                 (lyric text, charsNumber int, versesNumber int, linesNumber int, linesPerVerse int)
+                 (artist text, title text, lyric text, charsNumber int, versesNumber int, linesNumber int, linesPerVerse int, wordNumbers int)
             ''')
     c.execute('''CREATE TABLE assoc
-                 (word text, times int, verseNumber int, nextWord int, precWord int)
+                 (artist text, title text, word text, times int, verseNumber int, nextWord text, precWord text)
             ''')
 
-if task == "download":
+if task == 'download':
     if item == "hits":
         ##import pprint #debug
         #Get the current year
@@ -55,9 +66,6 @@ if task == "download":
                     page[year] = requests.get(url).text
                     #print r.content
                     tree = html.fromstring(page[year])
-                    months = tree.xpath('//tr[@class="first_row"]/td/b/text()')
-                #    songs = tree.xpath('//a[@class="song_link"]/text()')
-                #    artists = tree.xpath('//a[@class="artist_link"]/text()')
                     list = tree.xpath('//td[@class="left"]//text()')
                     artists = []
                     songs = []
@@ -83,17 +91,141 @@ if task == "download":
                             
                         to_add = [year,artist,songs[num],number,month_counter]
                         db.execute('INSERT INTO hits VALUES ( ?,?,?,?,? )', to_add)
-
                         num += 1
                 except:
-                    print "Network error, try again"
+                    print network_error_message
     else:
-        #Download lyrics
-        print "Scarica testi"            
+        print "Lyrics Download"
+        c.execute('SELECT artist,title FROM hits')
+        songs = c.fetchall()
+        
+        for song in songs:
+            query = song[0] + ' ' + song[1]
+            query = query.encode('utf-8')
+            artist = urllib.quote_plus(song[0].encode('utf-8'))
+            song_title = urllib.quote_plus(song[1].encode('utf-8'))
+            c.execute('SELECT 1 FROM lyrics WHERE `artist` = ? AND `title` = ?', (artist,song_title,))
+            if c.fetchone():
+                print "Lyrics %s from %s already present, skipping" % (song[1], song[0])
+                continue
+            else:
+              #  try:
+                #Download lyrics
+                print "Looking for %s ..." % query,
+                # TODO: if not found, try to replace & with and etc
+                # Or removing "featuring" etc
+                url = "http://lyrics.wikia.com/api.php?artist=%s&song=%s" % (artist,song_title)
+                page = requests.get(url).text
+                tree = html.fromstring(page)
+                in_page_search = tree.xpath('//body/h3/a')
+                song_url = in_page_search[0].attrib['href']
+                print "... downloading it"
+                page = requests.get(song_url).text
+                tree = html.fromstring(page)
+                song_text = tree.xpath('//div[@class="lyricbox"]/text()')
+                new_lines = tree.xpath('//div[@class="lyricbox"]//br')
+                
+                total_lines = 0
+                for br in new_lines:
+                    total_lines += 1
+                
+                song_text = [value for value in song_text if value != '\n']
+                
+                charlenght = 0
+                words_number = 0
+                for verse in song_text:
+                    verse.encode('utf-8')
+                    charlenght += count_letters(verse)
+                    words_number += len(verse) - count_letters(verse) + 1 #Count words as spaces +1
+                
+                #TODO: I lose a new line somewhere... but this way works
+                br_number = total_lines - len(song_text) + 1 + 1
+                
+                verse_lenght = str(song_text)
+                # (artist, title, lyric, charsNumber, versesNumber, linesNumber, linesPerVerse, wordNumbers)
+                to_add = [artist, song_title, str(song_text), charlenght, br_number, len(song_text),"", words_number]
+                db.execute('INSERT INTO lyrics VALUES ( ?,?,?,?,?,?,?, ? )', to_add)
+                db.commit()
 
+#                except:
+     #          print network_error_message
+
+elif task == 'analyse':
+    ##Add BPM, genre, mood
+    c.execute('SELECT artist,title,lyric FROM lyrics')
+    songs = c.fetchall()
+    
+    for song in songs:
+        artist = song[0]
+        title = song[1]
+        lyric = ast.literal_eval(song[2])  
+        d.execute('SELECT 1 FROM assoc WHERE artist = ? AND title = ?', (artist, title,))
+        if d.fetchone():
+            print "Lyrics %s from %s already present, skipping" % (title, artist)
+            continue
+        else:
+            print "Analysing %s from %s ..." % (title, artist),
+            for line in lyric:
+                a = ""
+                b = ""
+                c = ""
+                ##Works but is ugly. FIX
+                for word in line.split():
+                    a = b
+                    b = c
+                    c = clear_word(word)
+                    #(artist text, title text, word text, times int, verseNumber int, nextWord text, precWord text)
+                    to_add = [artist, title, b, "", "", c, a]
+                    if b:
+                        db.execute('INSERT INTO assoc VALUES ( ?,?,?,?,?,?,? )', to_add)
+                a = b
+                b = c
+                c = ""
+                to_add = [artist, title, b, "", "", c, a]
+                db.execute('INSERT INTO assoc VALUES ( ?,?,?,?,?,?,? )', to_add)
+            db.commit()
+            print "... done!"
+
+elif task == 'generate':
+    #Get mean line word lenght
+    c.execute('SELECT AVG(`wordNumbers`), AVG(`linesNumber`), AVG(`versesNumber`) from lyrics')
+    stats = c.fetchone()
+    if not stats:
+        print "Error with the db"
+        exit()
+    
+    meanWords = stats[0]
+    meanLines = stats[1]
+    meanVerse = stats[2]
+    meanLineLen = meanWords / meanLines
+    meanVerseLen = meanLines / meanVerse
+    print "Info: generating lines of lenght: %s words, %s verse lines, %s verses" % (meanLineLen, meanVerseLen, meanVerse)
+    
+    first_word = item
+    c.execute('SELECT `nextword` FROM assoc WHERE word LIKE ? ORDER BY RANDOM () LIMIT 1 ', (first_word,))
+    current_word = c.fetchone()[0]
+    prec_word = first_word
+    generated_line = first_word + " " + current_word
+    for gen_song in range (0, int(meanVerse)):
+        for gen_verse in range(0,int(meanVerseLen)):
+            for gen_word in range(0,int(meanLineLen)):
+                d.execute('SELECT `nextword` FROM assoc WHERE `word` LIKE ? AND `precWord` LIKE ? ORDER BY RANDOM () LIMIT 1 ', (current_word,prec_word,))
+                prec_word = current_word
+                current_word = d.fetchone()[0]
+                if not current_word:
+                    d.execute('SELECT `nextword` FROM assoc WHERE `word` LIKE ? ORDER BY RANDOM () LIMIT 1 ', (current_word,))
+                    prec_word = current_word
+                    current_word = d.fetchone()[0]
+                    
+                generated_line += " " + current_word
+                print generated_line
+        print '\n'
+
+
+    
 # Save (commit) the changes
 db.commit()
 
 # We can also close the connection if we are done with it.
 # Just be sure any changes have been committed or they will be lost.
-db.close()
+#db.close()
